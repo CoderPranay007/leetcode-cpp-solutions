6b539fead76018c5b66924b112fae58ce966c15c
CoderPranay007 <pranaykhera.pk@gmail.com>
2026-07-17 21:39:07 +0530

    Add Solution for LeetCode 3658
    gcd(sumOdd, sumEven) = n

---
+++ 3658_GCD_of_Odd_and_Even_Sums.cpp
@@ -0,0 +1,15 @@
+/*
+ * LeetCode 3658. GCD of Odd and Even Sums
+ * Difficulty: Easy
+ * Topics: Math, Number Theory
+ *
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ */
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
